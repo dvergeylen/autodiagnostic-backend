e50754d878de10103cdf8046dd1bc1782331e3af
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2021-06-07 16:56:50 +0200

    style: display DialogNodes lazily (hidden by default)

--- src/DialogNode.svelte
+++ src/DialogNode.svelte
@@ -4,19 +4,24 @@
   export let scene: Scenario;
   export let id: number;
   export let dialogNode: DialogNode;
+  let showChildNodes = false;
 
   const t = (text): string => {
     return text?.[language]?.[gender] || text?.[language]?.m || text?.[language] || text.fr;
   }
 
   const normalizedId = `${'0'.repeat(5-String(id).length)}${id}`;
+
+  function toggleShowChildNodes() { showChildNodes = !showChildNodes }
 </script>
 
 <li>
-  <p>
-    <strong>#{normalizedId}</strong>: {t(dialogNode.text)}
+  <p on:click={toggleShowChildNodes}>
+    <span class:hidden={showChildNodes}>▷</span>
+    <span class:hidden={!showChildNodes}>▽</span>
+    <strong>#{normalizedId}</strong>: {t(dialogNode.text)} <span class="is-primary">({dialogNode.nextNodes.length})</span>
   </p>
-  <ul>
+  <ul class:hidden={!showChildNodes}>
     {#each dialogNode.nextNodes as nodeId, i (i)}
       {#if !scene.dialogNodes[nodeId].isTerminal}
         <svelte:self id={nodeId} {scene} dialogNode={scene.dialogNodes[nodeId]} {gender} {language}/>
@@ -26,10 +31,18 @@
 </li>
 
 <style>
-p {
-  list-style: none;
-  text-align: left;
-  border: 1px black solid;
-  padding: 0.5em;
-}
+  ul {
+    list-style: none;
+  }
+  p {
+    text-align: left;
+    border: 1px black solid;
+    padding: 0.5em;
+  }
+  .hidden {
+    display: none;
+  }
+  .is-primary {
+    color: rgb(0, 174, 255);
+  }
 </style>
