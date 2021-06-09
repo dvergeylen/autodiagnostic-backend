a246a44c3d5241552bf58931c28925a968d2dd46
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2021-06-09 09:51:24 +0200

    style: enhanced node hierarchy lisibility

--- src/DialogNode.svelte
+++ src/DialogNode.svelte
@@ -4,30 +4,49 @@
   export let scene: Scenario;
   export let nodeId: number;
   let dialogNode: DialogNode;
-  let showChildNodes = false;
+  let showChildNodes: boolean = false;
+  let multipleIncomingNodes: boolean = false;
+  let readonly: boolean = false;
+  let displaySettings: boolean = false;
 
   const t = (text): string => {
     return text?.[language]?.[gender] || text?.[language]?.m || text?.[language] || text.fr;
   }
 
-  const normalizedId = `${'0'.repeat(5-String(nodeId).length)}${nodeId}`;
+  const normalizedId: string = `${'0'.repeat(5-String(nodeId).length)}${nodeId}`;
 
   function toggleShowChildNodes() { showChildNodes = !showChildNodes }
 
   $: dialogNode = scene.dialogNodes[nodeId];
+  $: multipleIncomingNodes = (dialogNode?.incomingNodes?.length > 1 ) || false;
+  $: readonly = dialogNode.isTerminal || dialogNode?.incomingNodes?.length > 1;
 </script>
 
-<li>
-  <p on:click={toggleShowChildNodes}>
-    <span class:hidden={showChildNodes}>▷</span>
-    <span class:hidden={!showChildNodes}>▽</span>
-    <strong>#{normalizedId}</strong>: {t(dialogNode.text)} <span class="is-primary">({dialogNode.nextNodes.length})</span>
+<li id="{normalizedId}">
+  <p class:is-player={dialogNode.character === "Player"}>
+    {#if !readonly}
+      <span on:click={toggleShowChildNodes} class:is-hidden={showChildNodes}>▷</span>
+      <span on:click={toggleShowChildNodes} class:is-hidden={!showChildNodes}>▽</span>
+    {:else}
+      <span title="Noeud terminal">¤</span>
+    {/if}
+    <strong on:click={toggleShowChildNodes}>{dialogNode.character}</strong>:
+    <span  on:click={toggleShowChildNodes} class:italic={readonly}>{t(dialogNode.text)}</span>
+
+    (<span class:is-hidden={!multipleIncomingNodes} class="is-primary"
+          title="{dialogNode.incomingNodes?.length || 0} répliques mène(nt) à ce noeud">
+          ⇶ {dialogNode.incomingNodes?.length || 0} - 
+      </span>
+      <span class="is-primary"
+       title="Ce noeud mène à {dialogNode.nextNodes.length} réplique(s)">
+      💬 {dialogNode.nextNodes.length}
+      </span>
+    )
   </p>
-  <ul class:hidden={!showChildNodes}>
+
+  <ul class:is-hidden={!showChildNodes}>
     {#each dialogNode.nextNodes as nodeId, i (i)}
-      {#if !scene.dialogNodes[nodeId].isTerminal}
-        <svelte:self {nodeId} {scene} {gender} {language}/>
-      {/if}
+      <svelte:self {nodeId} {scene} {gender} {language}/>
     {/each}
   </ul>
 </li>
@@ -41,10 +60,16 @@
     border: 1px black solid;
     padding: 0.5em;
   }
-  .hidden {
+  .is-hidden {
     display: none;
+  }
+  .italic {
+    font-style: italic;
   }
   .is-primary {
     color: rgb(0, 174, 255);
+  }
+  .is-player {
+    background-color: #f2f9ff;
   }
 </style>
